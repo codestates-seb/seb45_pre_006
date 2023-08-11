e4287670cb5cd6d1f5bd57ceb066d6ba035696be
myeongin <mungin0926@naver.com>
2023-08-11 17:18:58 +0900

    [Design] #10 - 헤더 디자인

--- client/src/hooks/useForm.jsx
+++ client/src/hooks/useForm.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-
-const useForm = () => {
-  return {};
-};
-
-export default useForm;
+export default function useForm(data) {
+  const [form, setForm] = useState(data);
+  const onInputChangeHandler = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const clearForm = () => setForm(data);
+  return [form, onInputChangeHandler, clearForm];
+}
