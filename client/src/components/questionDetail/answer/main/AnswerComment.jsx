265d0d4be85d2c85a3589fe01d6c09ed3056ec43
WONHO22 <dnjsgh2122@gmail.com>
2023-08-18 01:07:04 +0900

    [Feat] #7, #9 - 질문, 답변 댓글 5개 초과시 조건부 렌더링 구현완료

--- client/src/components/questionDetail/answer/main/AnswerComment.jsx
+++ client/src/components/questionDetail/answer/main/AnswerComment.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
 import useForm from "../../../../hooks/useForm";
 import getWriteDate from "../../../common/getWriteDate";
@@ -33,9 +33,29 @@ const StyleAnswerComment = styled.div`
   .createdat {
     color: var(--black-400);
   }
+  .showMoreButton {
+    border-top: 1px solid var(--border);
+    color: var(--blue-600);
+    padding: 10px;
+    cursor: pointer;
+    &:hover {
+      color: var(--blue-500);
+    }
+    > span {
+      font-weight: 700;
+    }
+  }
 `;
 
 export default function AnswerComment({ data }) {
+  // 상태 변수 추가
+  const [showAllComments, setShowAllComments] = useState(false);
+
+  // 댓글을 더 보여주기 위한 함수
+  const handleShowMoreComments = () => {
+    setShowAllComments(true);
+  };
+
   // input창 (댓글) 작성 부분
   const initialInputData = {
     comment: "",
@@ -52,19 +72,29 @@ export default function AnswerComment({ data }) {
     }
   };
 
+  const postData = data.comment;
+
   return (
     <StyleAnswerComment>
-      <div className="comment-list">
-        {data.comment.map((comment, idx) => (
-          <div key={idx} className="comentlist">
-            <span className="commentbody">{comment.commentBody} - </span>
-            <span className="username"> {comment.username}</span>
-            <span className="createdat">{getWriteDate(comment.createdAt)}</span>
-          </div>
-        ))}
-      </div>
-
+      {console.log(postData)}
+      {postData.map(
+        (data, idx) =>
+          // 5개까지만 표시
+          ((!showAllComments && idx < 5) || showAllComments) && (
+            <div key={idx} className="comentlist">
+              <span className="commentbody">{data.commentBody} - </span>
+              <span className="username"> {data.username}</span>
+              <span className="createdat">{getWriteDate(data.createdAt)}</span>
+            </div>
+          )
+      )}
       {/* 5개 이상일경우 Show ~ more comments 렌더링 */}
+      {postData.length > 5 && !showAllComments && (
+        <div className="showMoreButton" onClick={handleShowMoreComments}>
+          Show <span>{postData.length - 5}</span> more comments
+        </div>
+      )}
+
       <input
         type="text"
         name="comment"
